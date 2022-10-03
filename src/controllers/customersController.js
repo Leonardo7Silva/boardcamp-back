3f9255dda9fefe72eaae19c55a1b9e127a26bdcf
Leonardo7Silva <107073892+Leonardo7Silva@users.noreply.github.com>
2022-10-03 14:11:13 -0300

    Update customersController.js

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -72,7 +72,7 @@ async function updateCustomer(req, res){
 
     try{
         const cpfCustomers = await connection.query('SELECT * FROM customers WHERE cpf = $1;',[cpf]);
-        if(cpfCustomers.rows.length > 0 ){
+        if(cpfCustomers.rows.length > 1 ){
             return res.sendStatus(409);
         }
         
